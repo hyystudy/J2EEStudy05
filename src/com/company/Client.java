9201c352589985955bf114f5e2d372a208c4506d
hyy <1501119596@163.com>
2018-04-03 16:16:26 +0800

    done

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+
+    public static void main(String[] args){
+
+        try {
+            Socket socket = new Socket("127.0.0.1", 8888);
+
+            //构建IO
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            //将当前目录的test.txt文件上传到服务器
+            //下面上传的图片到服务器打不开
+           /* BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("ic_launcher_round.png")));
+            //向服务器发信息
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+            String string;
+            while ((string = br.readLine()) != null) {
+                System.out.println(string);
+                bufferedWriter.write(string);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }*/
+
+           //第二种 以字节流的形式读写
+            FileInputStream fileInputStream = new FileInputStream("ic_launcher_round.png");
+            byte[] bytes = new byte[1024];
+            int len;
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+            }
+            outputStream.flush();
+
+
+            //读取 服务器返回的信息
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String msg = bufferedReader.readLine();
+            System.out.println(msg);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,6 +1,48 @@
 package com.company;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+    public static void main(String[] args) {
 
+        try{
+            ServerSocket serverSocket = new ServerSocket(8888);
+            System.out.println("启动服务器。。。。");
+            Socket client = serverSocket.accept();
+            System.out.println("Client : " + client.getInetAddress().getHostName() + " connected to server");
+
+            InputStream clientInputStream = client.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientInputStream));
+            //将客户端上传的数据保存到服务器 D盘的test.txt
+            /*//下面这种方式将客户端的png图片保存到服务器端 但出现打不开png图片
+            BufferedWriter bufferedWriter1 = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\ic_launcher.png")));
+            //read client msg
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                bufferedWriter1.write(s);
+                bufferedWriter1.newLine();
+                System.out.println(s);
+                bufferedWriter1.flush();
+            }*/
+            //第二种方式 以字节流的形式读写
+            byte[] bytes = new byte[1024];
+            int len;
+            FileOutputStream fileOutputStream = new FileOutputStream("D:\\hyy.png");
+            while ((len = clientInputStream.read(bytes)) != -1) {
+                fileOutputStream.write(bytes, 0 , len);
+            }
+            fileOutputStream.flush();
+
+
+           /* BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            bufferedWriter.write(s + "\n");
+            bufferedWriter.flush();*/
+        }catch (Exception e){
+
+        }
+    }
 }
